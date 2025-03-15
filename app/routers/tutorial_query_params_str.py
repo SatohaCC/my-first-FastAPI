9108786c3f75188f55befaf4f9b4593b9d15c8f6
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-03-16 00:04:37 +0900

    PUTリクエストの処理を改善し、アイテムのID、クエリパラメータ、リクエストボディのバリデーションを追加。新しいルーターを更新し、エンドポイントの動作を強化。

---
+++ app/routers/tutorial_query_params_str.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+
+from typing import Union
+
+router = APIRouter()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+@router.get("/items/")
+async def read_items(q: str | None = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
