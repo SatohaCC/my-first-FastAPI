7f7a8d1f800f17557cb852396cfe5eb372a11068
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-03-24 20:15:38 +0900

    add :model_config

--- app/routers/tutorial_query_param_models.py
+++ app/routers/tutorial_query_param_models.py
@@ -8,6 +8,8 @@ router = APIRouter()
 
 
 class FilterParams(BaseModel):
+    model_config = {"extra": "forbid"}
+
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
     order_by: Literal["created_at", "updated_at"] = "created_at"
